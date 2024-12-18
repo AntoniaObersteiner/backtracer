ec1b51130d789aefc72d73f675907844c3c6939e
AntoniaObersteiner <antonia.obersteiner@gmail.com>
2024-12-18 16:39:58 +0100

    move from very large buffer to mmap

--- server/src/interpret.cpp
+++ server/src/interpret.cpp
@@ -6,6 +6,9 @@
 #include <memory>
 #include <format>
 #include <regex>
+#include <sys/stat.h>
+#include <sys/mman.h>
+#include <fcntl.h>
 
 #include "elfi.hpp"
 
@@ -450,7 +453,7 @@ int main(int argc, char * argv []) {
 	}
 
 	std::cout << std::endl;
-	uint64_t buffer[BUFFER_CAPACITY_IN_WORDS];
+
 
 	if (argc < 2) {
 		throw std::runtime_error("missing args: need input file (.btb)");
@@ -458,29 +461,45 @@ int main(int argc, char * argv []) {
 
 	std::string filename { argv[1] };
 	printf("reading file '%s'\n", filename.c_str());
-	std::ifstream file (filename, std::ios::binary);
-	if (!file.is_open()) {
-		throw std::runtime_error("failed opening '" + filename + "'!");
-	}
 
-	char * read_alias = reinterpret_cast<char *>(&buffer[0]);
-	size_t capacity = BUFFER_CAPACITY_IN_WORDS * sizeof(uint64_t);
+	size_t buffer_size_in_bytes, buffer_size_in_words;
+	uint64_t * buffer;
 
-	file.read(read_alias, capacity);
-	if (file.eof()) {
-		printf("file read exhaustively!\n");
-	} else {
-		if (!file) {
-			throw std::runtime_error("failed reading from '" + filename + "'!");
-		} else {
-			printf("file is longer than %ld bytes. that is not yet implemented!.\n", capacity);
+	{
+		struct stat stat_buffer;
+		if (stat(filename.c_str(), &stat_buffer) != 0) {
+			perror(filename.c_str());
+			throw std::runtime_error("could not execute stat on '" + filename + "'!");
 		}
+		buffer_size_in_bytes = stat_buffer.st_size;
+		if (buffer_size_in_bytes % sizeof(unsigned long)) {
+			throw std::runtime_error(
+				"file '" + filename + "' is " + std::to_string(buffer_size_in_bytes)
+				+ " long, which is not a whole number of words ("
+				+ std::to_string(sizeof(unsigned long)) + " bytes / word)!"
+			);
+		}
+		buffer_size_in_words = buffer_size_in_bytes / sizeof(unsigned long);
+
+		printf(
+			"file '%s' is %ld bytes, %ld words long.",
+			filename.c_str(), buffer_size_in_bytes, buffer_size_in_words
+		);
+
+		// only needed to create mmap mapping
+		int fd = open(filename.c_str(), O_RDONLY);
+		void * raw_buffer = mmap(0, buffer_size_in_bytes, PROT_READ, MAP_PRIVATE, fd, 0);
+		close(fd);
+
+		if (raw_buffer == reinterpret_cast<void *>(-1)) {
+			perror("mmap");
+			throw std::runtime_error("could not mmap '" + filename + "'!");
+		}
+
+		buffer = reinterpret_cast<uint64_t *>(raw_buffer);
 	}
-	size_t length_in_words = file.gcount();
 
-	printf("read %ld words from file '%s'\n", length_in_words, filename.c_str());
-
-	EntryArray entry_array(RawEntryArray (buffer, length_in_words));
+	EntryArray entry_array(RawEntryArray (buffer, buffer_size_in_words));
 
 	for (const auto & entry : entry_array) {
 		printf("read entry: \n%s\n", entry.to_string().c_str());
