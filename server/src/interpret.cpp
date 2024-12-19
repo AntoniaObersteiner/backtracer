dd4c96f099b8ae94e4fea1cd1f50bedec13483db
AntoniaObersteiner <antonia.obersteiner@gmail.com>
2024-12-19 13:40:05 +0100

    update long-out-of-date entry_types enum

--- server/src/interpret.cpp
+++ server/src/interpret.cpp
@@ -19,8 +19,7 @@ enum entry_types {
 	BTE_STACK    = 1 << 0,    // normal stack trace
 	BTE_MAPPING  = 1 << 1,    // library mapping info
 	BTE_INFO     = 1 << 2,    // information about module
-	BTE_START    = 1 << 3,    // start entry
-	BTE_STOP     = 1 << 4,    // stop entry
+	BTE_CONTROL  = 1 << 3,    // start/stop(/reset) entry
 };
 
 // a string like 'tsc_duration' that descibes an entry attribute
