cdd6eb3a62926bc291ccdb7177b5d0deed18f947
AntoniaObersteiner <antonia.obersteiner@gmail.com>
2024-12-29 00:28:06 +0100

    finish test of compression

--- server/src/test_compress.cpp
+++ server/src/test_compress.cpp
@@ -1,3 +1,4 @@
+#include <iomanip>
 #include <iostream>
 #include <format>
 #include <string>
@@ -9,11 +10,11 @@ template <typename T, size_t L>
 void dump (std::string note, const std::span<T, L> & data) {
 	std::cout << note << std::endl;
 
-	const int values_per_line = 64 / sizeof(T);
+	const int values_per_line = 100 / (2 * sizeof(T) + 1);
 	int already_on_line = 0;
 
 	for (const auto & value : data) {
-		std::cout << std::format("{:8x}", value);
+		std::cout << std::hex << std::setw(2 * sizeof(T)) << static_cast<int>(value);
 
 		if (already_on_line >= values_per_line) {
 			std::cout << std::endl;
@@ -40,7 +41,7 @@ int main() {
 			raw_data[i] = i;
 	}
 	std::vector<uint8_t> compressed;
-	compressed.reserve(raw_data.size() * sizeof(uint64_t) * 2);
+	compressed.resize(raw_data.size() * sizeof(uint64_t) * 2);
 
 	std::array<uint64_t, dictionary_capacity> dictionary;
 	dump_wrap("raw_data:", raw_data);
@@ -55,5 +56,28 @@ int main() {
 		std::cout << "could not compress into result array!" << std::endl;
 	}
 
-	dump_wrap("compressed:", compressed);
+	std::span compressed_cut { compressed.data(), compressed_size };
+
+	dump_wrap("compressed:", compressed_cut);
+
+	auto decompressed = decompress(compressed_cut, dictionary);
+
+	dump_wrap("decompressed:", decompressed);
+
+	if (raw_data.size() != decompressed.size()) {
+		std::cout << "raw_data and decompressed differ in length, "
+			"therefore cannot be equal" << std::endl;
+		return 1;
+	}
+	auto [raw_mismatch, decomp_mismatch] = std::mismatch(raw_data.begin(), raw_data.end(), decompressed.begin());
+	if (raw_mismatch != raw_data.end()) {
+		int index = raw_mismatch - raw_data.begin();
+		std::cout
+			<< "raw_data and decompressed differ at " << index
+			<< ", raw_data: " << raw_data[index]
+			<< ", decompressed: " << decompressed[index]
+			<< std::endl;
+	} else {
+		std::cout << "no difference found, decompression works!" << std::endl;
+	}
 }
