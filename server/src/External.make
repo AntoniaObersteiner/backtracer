2e99426d24d54c1c4290e636dde8ec599c0abad3
AntoniaObersteiner <antonia.obersteiner@gmail.com>
2024-12-30 02:16:07 +0100

    add decompression step into pipelines

--- server/src/External.make
+++ server/src/External.make
@@ -52,6 +52,7 @@ SAMPLE_PATH=$(BASE_PATH)/$(SAMPLE_RELPATH)
 
 SAMPLE:=$(SAMPLE_PATH)/$(MODULE).traced
 CLEANED:=$D/$(MODULE).cleaned
+COMPRESSED:=$D/$(MODULE).compressed
 BUFFER:=$D/$(MODULE).btb
 INTERPRETED:=$D/$(MODULE).interpreted
 
@@ -69,14 +70,17 @@ interpret: $(CXXOBJECTS) $(CXXHEADERS)
 	$(CXX) -o $@ $(CXXOBJECTS) $(CXXFLAGS)
 test_compress: $O/test_compress.o $O/compress.o $S/compress.hpp
 	$(CXX) -o $@ $(filter %.o,$+)
+decompress: $O/decompress.o $O/compress.o $O/mmap_file.o $S/compress.hpp
+	$(CXX) -o $@ $(filter %.o,$+)
 
 .NOTINTERMEDIATE:
 
 $(SAMPLE_RELPATH)/%.traced:
 $D/%.cleaned:
+$D/%.compressed:
+$D/%.btb:
 $D/%.folded:
 $D/%.interpreted:
-$D/%.btb:
 
 $O/%.o: $S/%.cpp
 	$(CXX) $< -c -o $@ $(CXXFLAGS)
@@ -95,9 +99,13 @@ $(SAMPLE_PATH)/%.traced:
 $D/%.cleaned: $(SAMPLE_PATH)/%.traced
 	cat -v $< > $@
 
-%.btb: %.cleaned unpack
-	# unpack the printed hex to the backtrace buffer binary format
+%.compressed: %.cleaned unpack
+	# unpack the printed hex to the compressed btb format
 	./unpack $< $@
+
+%.btb: %.compressed decompress
+	# decompress to the backtrace buffer binary format
+	./decompress $< $@
 
 %.interpreted: %.btb interpret $(BINARY_LIST)
 	# interpret the backtrace buffer binary format and write to $@ file, log to $@.log
@@ -128,7 +136,7 @@ sample_length:
 .PHONY: gdb
 gdb_unpack: unpack $(CLEANED)
 	echo "b main" > test_unpack.gdb
-	echo "run $(CLEANED) $(BUFFER) > ./stdout 2> ./stderr" >> test_unpack.gdb
+	echo "run $(CLEANED) $(COMPRESSED) > ./stdout 2> ./stderr" >> test_unpack.gdb
 
 	gdb -tui --command=test_unpack.gdb ./unpack
 
@@ -148,6 +156,13 @@ gdb_compress: test_compress
 
 	gdb -tui --command=test_compress.gdb ./test_compress
 
+.PHONY: gdb_decompress
+gdb_decompress: test_compress $(COMPRESSED)
+	echo "b main" > test_decompress.gdb
+	echo "run $(COMPRESSED) $(BUFFER) > ./stdout 2> ./stderr" >> test_decompress.gdb
+
+	gdb -tui --command=test_decompress.gdb ./decompress
+
 $(ELFDUMP):
 	cd $(BASE_PATH)/ELFIO/; cmake .
 	make -C $(BASE_PATH)/ELFIO/ elfdump
@@ -165,7 +180,7 @@ $O/%.disas:
 clean:
 	# note: does not remove .cleaned or .traced files, because tracing takes so long
 	rm -f \
-		$D/*.btb $D/*.interpreted $D/*.folded $D/*.svg \
+		$D/*.btb $D/*.compressed $D/*.interpreted $D/*.folded $D/*.svg \
 		./stderr ./stdout \
 		./unpack ./interpret \
 		$(CXXDEPENDENCIES) $(CXXOBJECTS)
---
+++ server/src/src/decompress.cpp
@@ -0,0 +1,105 @@
+#include <iomanip>
+#include <iostream>
+#include <format>
+#include <fstream>
+#include <string>
+#include <vector>
+
+#include "compress.hpp"
+#include "mmap_file.hpp"
+
+template <typename T, size_t L>
+void dump (std::string note, const std::span<T, L> & data) {
+	std::cout << note << std::endl;
+
+	const int values_per_line = 100 / (2 * sizeof(T) + 1);
+	int already_on_line = 0;
+
+	for (const auto & value : data) {
+		std::cout << std::hex << std::setw(2 * sizeof(T)) << static_cast<int>(value);
+
+		if (already_on_line >= values_per_line) {
+			std::cout << std::endl;
+			already_on_line = 0;
+		} else {
+			already_on_line ++;
+			std::cout << " ";
+		}
+	}
+	std::cout << std::endl;
+}
+template <typename Container>
+void dump_wrap (std::string note, const Container & data) {
+	dump(note, std::span(data));
+}
+
+void write_out(std::string filename, std::span<const uint64_t> data) {
+	std::ofstream output_stream {
+		filename,
+		std::ios::out
+		| std::ios::trunc
+		| std::ios::binary
+	};
+
+	for (const auto & word : data) {
+		output_stream.write(reinterpret_cast<const char *>(&word), sizeof(uint64_t));
+	}
+}
+
+int main(int argc, char * argv []) {
+	if (argc < 3) {
+		printf(
+			"usage: decompress <input> <output>\n"
+			"usually, input ends in .compressed and output in .btb\n"
+		);
+		exit(1);
+	}
+
+	std::string input_filename { argv[1] };
+	std::string output_filename { argv[2] };
+
+	uint64_t * input_buffer;
+	size_t input_size_in_words;
+	mmap_file(input_filename, input_buffer, input_size_in_words);
+
+	std::span input { input_buffer, input_size_in_words };
+
+	// parse the compression_header
+	size_t header_capacity_in_words = (sizeof(compression_header_t) - 1) / sizeof(uint64_t) + 1;
+	if (input_size_in_words < header_capacity_in_words) {
+		printf("input file '%s' is smaller than even the compression header??\n", input_filename.c_str());
+		exit(1);
+	}
+
+	compression_header_t * compression_header = reinterpret_cast<compression_header_t *> (input_buffer);
+	const uint64_t * dictionary_raw = input_buffer + compression_header->dictionary_offset;
+	const size_t dictionary_length_in_words = compression_header->dictionary_length;
+
+	const uint64_t * compressed_raw = dictionary_raw + dictionary_length_in_words;
+	const size_t compressed_length_in_bytes = compression_header->data_length_in_bytes;
+
+	const std::span dictionary { dictionary_raw, dictionary_length_in_words };
+	const std::span compressed { reinterpret_cast<const uint8_t*>(compressed_raw), compressed_length_in_bytes };
+
+	if (!compression_header->is_compressed) {
+		if (compressed_length_in_bytes % sizeof(uint64_t) != 0) {
+			printf("data is supposedly not compressed, but length is not multiple of word length??\n");
+			exit(1);
+		}
+
+		size_t compressed_words = compressed_length_in_bytes / sizeof(uint64_t);
+		printf("data was not compressed, writing %ld words to '%s'\n", compressed_words, output_filename.c_str());
+		const std::span decompressed { compressed_raw, compressed_words };
+		write_out(output_filename, decompressed);
+		return 0;
+	}
+
+	auto decompressed = decompress(compressed, dictionary);
+
+	printf(
+		"data decompressed from %ld B, writing %ld words to '%s'\n",
+		compressed_length_in_bytes, decompressed.size(), output_filename.c_str()
+	);
+	write_out(output_filename, std::span { decompressed });
+}
+
