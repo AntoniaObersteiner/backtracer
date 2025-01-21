e5454ece122e8e369e89726e35cb76854622e255
AntoniaObersteiner <antonia.obersteiner@gmail.com>
2025-01-21 16:49:01 +0100

    add timing of start and stop

--- server/src/main.cc
+++ server/src/main.cc
@@ -23,6 +23,8 @@
 #include "src/btb_syscall.h"
 
 int main(void) {
+	l4_cpu_time_t time_main = l4_rdtsc ();
+
 	l4_cap_idx_t dbg_cap = L4_BASE_DEBUGGER_CAP;
 	bool is_valid = l4_is_valid_cap(dbg_cap) > 0;
 	printf(">>> dbg_cap %ld is %svalid <<<\n", dbg_cap, is_valid ? "" : "not ");
@@ -33,17 +35,29 @@ int main(void) {
 
 	printf("wait a second...");
 	sleep(1);
+
 	l4_debugger_backtracing_set_timestep(dbg_cap, 10);
 	l4_debugger_backtracing_start(dbg_cap);
+	l4_cpu_time_t time_start = l4_rdtsc ();
 
 	printf("trace for some time...\n");
-	sleep(10);
+	sleep(1);
 	l4_debugger_backtracing_stop(dbg_cap);
+	l4_cpu_time_t time_stop = l4_rdtsc ();
 
 	unsigned long remaining_words;
 	do {
 		remaining_words = export_backtrace_buffer_section(dbg_cap, false, true);
 	} while (remaining_words);
+
+	l4_uint64_t us_main  = l4_tsc_to_us (time_main);
+	l4_uint64_t us_start = l4_tsc_to_us (time_start);
+	l4_uint64_t us_stop  = l4_tsc_to_us (time_stop);
+
+	printf("abs time main %8lx us\n", us_main);
+	printf("=?=?= [start] %8lx us\n", us_start - us_main);
+	printf("=?=?= [stop]  %8lx us\n", us_stop  - us_main);
+	printf("start to stop %8lx us\n", us_stop  - us_start);
 
 	printf(
 		"====================\n"
@@ -51,5 +65,6 @@ int main(void) {
 		"====================\n"
 	);
 	l4_platform_ctl_system_shutdown(pfc_cap, 0);
+
 	printf("everything exported, press Ctrl-A and then X\n");
 }
