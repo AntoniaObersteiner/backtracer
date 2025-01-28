edeff09511a6e00d8ca95303c99947da6a4a491c
AntoniaObersteiner <antonia.obersteiner@gmail.com>
2025-01-28 10:36:57 +0100

    provide default header for overhead measurement

---
+++ include/measure_defaults.h
@@ -0,0 +1,4 @@
+// this file is written by measure.py to automate overhead measurements
+static const l4_uint64_t ms_to_us = 1000;
+static const l4_uint64_t us_sleep_before_tracing = 120 * ms_to_us;
+static const l4_uint64_t us_trace_interval = 100 * ms_to_us;
