c8c30c09f2ed09a373906c8caa08bdeddc31a0b8
AntoniaObersteiner <antonia.obersteiner@gmail.com>
2025-02-09 23:50:19 +0100

    nicer histogram plots

--- server/src/tools/hist_plot.py
+++ server/src/tools/hist_plot.py
@@ -47,6 +47,7 @@ def main():
 
     # convert to µs
     unit = "µs"
+    unit_text = "µs"
     unit_factor = 1000
     data[f"average_time_in_{unit}"] = data["average_time_in_ns"] / unit_factor
 
@@ -57,10 +58,13 @@ def main():
     data = data.query(f"depth_min <= {max_depth_min}")
     print(data)
 
+    remove_outliers = False
     linear_estimator, linear_estimator_description = make_estimator(data, unit)
-    estimations = linear_estimator(data["depth_min"])
-    non_outlier_data = data[(data[f"average_time_in_{unit}"] - estimations).abs() <= 1000 / unit_factor]
-    linear_estimator, linear_estimator_description = make_estimator(non_outlier_data, unit)
+    if remove_outliers:
+        estimations = linear_estimator(data["depth_min"])
+        # the decision, what is and isn't an outlier is very unmethodic
+        non_outlier_data = data[(data[f"average_time_in_{unit}"] - estimations).abs() <= 1000 / unit_factor]
+        linear_estimator, linear_estimator_description = make_estimator(non_outlier_data, unit)
 
     # make sure the x-axis ticks are integer and rougly 10
     x_plot_width = data.shape[0]
@@ -70,7 +74,7 @@ def main():
     de = {
         "title": f"Dauer je Trace in Anwendung {app}",
         "xlabel": f"Stack-Tiefe in Frames",
-        "rlabel": f"Durchschnittliche Dauer in {unit}",
+        "rlabel": f"Durchschnittliche Dauer in {unit_text}",
         "elabel": f"Lineare Näherung der Dauer",
         "llabel": f"Anzahl mit dieser Stacktiefe",
         "depth": "Stack-Tiefe",
@@ -78,26 +82,44 @@ def main():
     en = {
         "title": f"duration per tracing of application {app}",
         "xlabel": f"stack depth in frames",
-        "rlabel": f"average trace time in {unit}",
+        "rlabel": f"average trace time in {unit_text}",
         "elabel": f"linear approximation of the durations",
         "llabel": f"number of traces of this depth",
         "depth": "depth",
     }
     lang = de
 
+    plt.style.use('seaborn-v0_8')
+    # print(plt.rcParams.keys())
+    params = {
+        "text.usetex" : False,
+        #"font.family" : "serif",
+        "font.serif" : ["Computer Modern Serif"] + plt.rcParams["font.serif"],
+        "font.sans-serif" : ["TeX Gyre Schola"] + plt.rcParams["font.sans-serif"],
+    }
+    plt.rcParams.update(params)
+
+    # make background, so grid is behind bars but matches duration ticks
+    ax_bg = sns.scatterplot(
+        data = data.query("count != 0"),
+        x = "depth_min",
+        y = f"average_time_in_{unit}",
+        color = "black",
+    )
 
     # plot backtround histogram of how many stacks per bin
-    ax = sns.barplot(
+    ax_bar = ax_bg.twinx()
+    sns.barplot(
         data = data,
         x = "depth_min",
         y = "count",
+        ax = ax_bar,
         label = lang["llabel"],
     )
 
-    #sns.set_style("whitegrid")
     # make another plot in front for linear regression
-    ax_est = ax.twinx()
-    lines_est = sns.lineplot(
+    ax_est = ax_bg.twinx()
+    sns.lineplot(
         x = data["depth_min"],
         y = linear_estimator(data["depth_min"]),
         ax = ax_est,
@@ -107,40 +129,53 @@ def main():
 
     #sns.set_style("white")
     # make another plot in front for average duration
-    ax_twin = ax.twinx()
-    lines_twin = sns.scatterplot(
+    ax_dot = ax_bg.twinx()
+    sns.scatterplot(
         data = data.query("count != 0"),
         x = "depth_min",
         y = f"average_time_in_{unit}",
-        ax = ax_twin,
+        ax = ax_dot,
         color = "black",
         label = lang["rlabel"],
     )
 
-    ax     .set_ymargin(.3)
-    ax     .xaxis.set_major_locator(mticker.MultipleLocator(tick_step))
-    ax_est .set_ybound(ax_twin.get_ybound())
+    ax_bar.set_ymargin(.3)
+    ax_bar.xaxis.set_major_locator(mticker.MultipleLocator(tick_step))
+    ax_bg .set_ybound(ax_dot.get_ybound())
+    ax_est.set_ybound(ax_dot.get_ybound())
+    ax_bg .grid(True)
+    ax_est.grid(False)
+    ax_dot.grid(False)
+    ax_bar.grid(False)
 
-    ax     .set_title(lang["title"])
-    ax     .set_xlabel(lang["xlabel"] + "\n" + lang["elabel"] + ": " + linear_estimator_description)
-    ax     .set_ylabel(lang["llabel"])
-    ax_twin.set_ylabel(lang["rlabel"])
-    ax_est .set_ylabel("")
+    ax_bg .set_title(lang["title"])
+    ax_bg .set_xlabel(lang["xlabel"] + "\n" + lang["elabel"] + ": " + linear_estimator_description)
+    ax_bar.set_ylabel(lang["llabel"])
+    ax_dot.set_ylabel(lang["rlabel"])
+    ax_bg .set_ylabel("")
+    ax_est.set_ylabel("")
     plt.subplots_adjust(bottom = .15)
+    ax_bg .tick_params(axis = "y", left = False, right = False, labelleft = False, labelright = False)
+    ax_bar.tick_params(axis = "y", left = True,  right = False, labelleft = True,  labelright = False)
+    ax_est.tick_params(axis = "y", left = False, right = False, labelleft = False, labelright = False)
+    ax_dot.tick_params(axis = "y", left = False, right = True,  labelleft = False, labelright = True)
+    ax_bar.yaxis.set_label_position("left")
+    ax_dot.yaxis.set_label_position("right")
 
-    h1,l1 = ax     .get_legend_handles_labels()
-    h2,l2 = ax_est .get_legend_handles_labels()
-    h3,l3 = ax_twin.get_legend_handles_labels()
-    ax_twin.legend(
+    h1,l1 = ax_bar.get_legend_handles_labels()
+    h2,l2 = ax_dot.get_legend_handles_labels()
+    h3,l3 = ax_est.get_legend_handles_labels()
+    ax_dot.legend(
         handles = h1 + h2 + h3,
         labels  = l1 + l2 + l3,
     )
-    ax    .get_legend().remove()
+    ax_bar.get_legend().remove()
     ax_est.get_legend().remove()
 
-    output_filename = args.filename + ".svg"
-    print(f"writing plot to {output_filename!r}")
-    plt.savefig(output_filename)
+    for ending in ["svg", "pdf"]:
+        output_filename = args.filename + "." + ending
+        print(f"writing plot to {output_filename!r}")
+        plt.savefig(output_filename)
 
 if __name__ == "__main__":
     main()
