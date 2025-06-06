2db66748b24e6fdd6241e3b82cd7289ad8fb49b9
AntoniaObersteiner <antonia.obersteiner@gmail.com>
2025-06-06 10:14:53 +0200

    more sane defaults

--- include/measure_defaults.h
+++ include/measure_defaults.h
@@ -4,14 +4,14 @@
 
 static const l4_uint64_t us_sleep_before_tracing = 120000;
 // in case of !app_controls_tracing, uses first. unit: 1  m  us
-static const l4_uint64_t us_trace_intervals [] =  {      10000 };
+static const l4_uint64_t us_trace_intervals [] =  {       1000 };
 // above numbers might be skewed by uninterruptible syscalls, where the tick counter does not ++
 static const l4_uint64_t trace_interval_count = 1;
 static const l4_uint64_t measure_rounds = 1;
 static const int do_overhead = 0;
 // for backtracer/main.cc
 static const int do_export = 1;
-static const int app_controls_tracing = 1;
+static const int app_controls_tracing = 0;
 static const int app_prints_steps = 0;
 
 // used when app_controls_tracing == 0.
@@ -24,4 +24,4 @@ static const int us_backtracer_waits_for_app = 20000000;
 static const int rounds_backtracer_waits_for_start = 20;
 
 // syscall debugging infos, no backtracer debugging infos
-static const int ubt_debug = 1;
+static const int ubt_debug = 0;
