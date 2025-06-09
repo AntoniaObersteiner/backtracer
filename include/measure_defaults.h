6db4079e5a66470318d97519d97637b6fbe38db9
AntoniaObersteiner <antonia.obersteiner@gmail.com>
2025-06-09 17:17:41 +0200

    commit current testing measure_defaults

--- include/measure_defaults.h
+++ include/measure_defaults.h
@@ -11,13 +11,13 @@ static const l4_uint64_t measure_rounds = 1;
 static const int do_overhead = 0;
 // for backtracer/main.cc
 static const int do_export = 1;
-static const int app_controls_tracing = 0;
+static const int app_controls_tracing = 1;
 static const int app_prints_steps = 0;
 
 // used when app_controls_tracing == 0.
 // the backtracer traces for this duration and then starts printing.
 //                                              s  m  u
-static const int us_backtracer_waits_for_app = 20000000;
+static const int us_backtracer_waits_for_app =  1000000;
 
 // used when app_controls_tracing = 1. how many rounds to wait for app to start.
 // useful when backtracer is scheduled after app and waiting for app to start backtracing is futile.
